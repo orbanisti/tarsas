117f30e0cd60bccb1dd27c0883c45cb87c315227
orbanisti <orbanisti94@gmail.com>
2021-06-01 16:17:12 +0200

    init commit

---
+++ assets/js/app.js
@@ -0,0 +1,27 @@
+// loads the Bootstrap jQuery plugins
+import 'jquery'
+import 'popper.js'
+import 'bootstrap'
+import '../scss/app.scss';
+// loads the code syntax highlighting library
+// import './highlight.js';
+
+// Creates links to the Symfony documentation
+import './doclinks.js';
+import {CountUp} from 'countup.js';
+require('animate.css')
+window.CountUp = CountUp;
+$(document).ready(function () {
+
+
+    const optionsCountUp = {
+        useEasing: true,
+        separator: ',',
+        decimal: '.'
+    };
+    $('.countup-numbers').each(function (index, element) {
+        let num = $(this).attr('data-num');
+        let counts = new CountUp(this, num, optionsCountUp);
+        counts.start();
+    });
+})
---
+++ src/Controller/FrontendController.php
@@ -0,0 +1,30 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class FrontendController extends AbstractController
+{
+
+    /**
+     * @Route("/", methods="GET", name="homepage")
+     */
+    public function homepage(): Response
+    {
+        return $this->render('frontend/homePage.html.twig');
+    }
+
+    /**
+     * @Route("/accounting", methods="GET", name="accounting")
+     */
+    public function accounting(): Response
+    {
+        return $this->render('frontend/accounting.html.twig');
+    }
+
+}
