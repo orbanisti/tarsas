8199d0d22316afb3da29777c5b1b2b1ce200b36d
orbanisti <orbanisti94@gmail.com>
2021-06-07 13:47:00 +0200

    adjusted sitemap

--- src/Controller/RedirectController.php
+++ src/Controller/RedirectController.php
@@ -28,7 +28,7 @@ class RedirectController extends AbstractController
             return new RedirectResponse($targetUrl, 301);
         }
 
-        return $this->render('frontend/404.html.twig',[],new Response('',404));
+        return $this->render('frontend/404.html.twig', [], new Response('', 404));
     }
 
 }
