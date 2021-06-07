acc9021cc6a4f47509302da1a1add6f56f4914f8
orbanisti <orbanisti94@gmail.com>
2021-06-07 12:05:27 +0200

    added seo markup + design changes

--- assets/js/app.js
+++ assets/js/app.js
@@ -42,3 +42,15 @@ $(document).ready(function () {
 
     })
 })
+if (window.innerWidth < 1050) {
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+}
+
