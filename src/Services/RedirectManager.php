d8923f2cfa5884324f3d3f4bb61bba42ba880ea1
orbanisti <orbanisti94@gmail.com>
2021-06-11 15:00:10 +0200

    redirected old blog urls (seo)

--- src/Services/RedirectManager.php
+++ src/Services/RedirectManager.php
@@ -4,6 +4,7 @@
 namespace App\Services;
 
 
+use App\Repository\PostRepository;
 use Symfony\Component\Routing\RouterInterface;
 
 class RedirectManager
@@ -16,28 +17,45 @@ class RedirectManager
         ];
     /** @var RouterInterface */
     private $router;
+    /** @var PostRepository */
+    private $postRepo;
 
     /**
      * RedirectManager constructor.
      *
      * @param   RouterInterface  $router
+     * @param   PostRepository   $postRepo
      */
-    public function __construct(RouterInterface $router)
+    public function __construct(RouterInterface $router, PostRepository $postRepo)
     {
-        $this->router = $router;
+        $this->router   = $router;
+        $this->postRepo = $postRepo;
     }
 
-
     /**
-     * @param $slug
+     * @param   string|null  $slug
      *
      * @return string
      */
-    public function getRedirectUrl($slug): ?string
+    public function getRedirectUrl(?string $slug): ?string
     {
         if (isset(self::$map[$slug])) {
             return $this->router->generate(self::$map[$slug]);
         }
+        $slugParts = explode('/', $slug);
+        /** Solves blog redirects from old page */
+        if (count($slugParts) == 2 && str_contains($slugParts[0], 'blog')) {
+            $post = $this->postRepo->createQueryBuilder('o')
+                                   ->andWhere('o.url LIKE :slug2')
+                                   ->setParameter('slug2', '%'.$slugParts[1].'%')
+                                   ->getQuery()
+                                   ->setMaxResults(1)
+                                   ->getSingleResult();
+            if ($post) {
+                return $this->router->generate('blog_post', ['url' => $post->getUrl()]);
+            }
+        }
+
 
         return null;
     }
